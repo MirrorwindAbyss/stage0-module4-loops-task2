f9c21ed4387937ab53cbd70e2c5cf242d404253c
Nabiyev Abdumalik <abdumalikunusov06@gmail.com>
2023-07-26 13:33:02 +0500

    Done!

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
-
+        int count=1;
+        int result=1;
+        while(count<=printToInclusive){
+            System.out.println(result=result*count++);
+        }
     }
+
+
 }
